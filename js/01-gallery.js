2c742276dd67bfcc2ce2202302df4056006994c1
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2022-11-21 01:22:26 +0200

    complete hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,56 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const images = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryItems(galleryItems);
+
+
+images.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const clickToImagesChange = e => {
+    console.log(e.target);
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+instance.show();
+
+
+    images.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+            instance.close();
+        }
+    })
+}
+
+
+
+
+
+images.addEventListener('click', clickToImagesChange);
+
+
+
+
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => 
+    {return `<div class="gallery__item">    
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>`}
+    ).join('');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const images = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryItems(galleryItems);
+
+images.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+
+let gallery = new SimpleLightbox('.gallery__item', { 
+    captionsData: 'alt',
+    captionDelay: 250 
+});
+
+
+
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => 
+    {return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`}
+    ).join('');
+}
